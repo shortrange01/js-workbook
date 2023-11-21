b309a65d242b586527e2c88869450f911bd3b6a0
hasegawa_taiji <hasegawa_taiji@cocone.co.jp>
2023-11-21 14:26:40 +0900

    feat: 回答を空にする

--- src/answer.ts
+++ src/answer.ts
@@ -1,7 +1,4 @@
 export const answer = (data: any): string | void => {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-    const dataArr = data[0].split("");
-    for (let i = 0; i < alphabet.length; i++) {
-        if (dataArr.indexOf(alphabet[i]) < 0) return alphabet[i];
-    }
+    let result: string = "";
+    return result;
 };
